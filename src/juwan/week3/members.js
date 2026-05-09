518044cc09701ad545aceb55ac557f7665263e87
mmnnbbnn <mmnnbbnn070910@gmail.com>
2026-05-09 23:36:32 +0900

    명단 추가, 삭제 인터랙티브 만들기

---
+++ src/juwan/week3/members.js
@@ -0,0 +1,99 @@
+import doeun from "../../assets/juwan/doeun.jpeg";
+import doyoung from "../../assets/juwan/doyoung.jpg";
+import somin from "../../assets/juwan/somin.png";
+import juwan from "../../assets/juwan/juwan.gif";
+import naham from "../../assets/juwan/naham.png";
+import seoyun from "../../assets/juwan/seoyun.png";
+import taewoo from "../../assets/juwan/taewoo.jpg";
+import taeyeop from "../../assets/juwan/taeyeop.png";
+
+export const members = [
+  {
+    name: "김주완",
+    role: "Frontend",
+    intro: "성실히 배우고 싶은 학생입니다.",
+    image: juwan,
+    badge: "React",
+    isMe: true,
+    email: "mmnnbbnn070910@gmail.com",
+    phone: "010-9041-1287",
+    website: "https://likelion.net/",
+    comment: "성실히 배워서 웹개발 마스터가 되고 싶습니다.",
+  },
+  {
+    name: "임도영",
+    role: "Frontend",
+    intro: "아기사자 14기 프론트엔드 임도영입니다.",
+    image: doyoung,
+    badge: "React",
+    email: "dlaehdud342@naver.com",
+    phone: "010-3516-6306",
+    website: "https://www.google.com",
+    comment: "꾸준히 노력하고 적극적인 참여를 통해 성장하는 개발자가 되겠습니다.",
+  },
+  {
+    name: "김나함",
+    role: "Frontend",
+    intro: "분야를 넘나들며 성장하는 개발자입니다.",
+    image: naham,
+    badge: "JavaScript",
+    email: "naham9488@gmail.com",
+    phone: "010-3626-9488",
+    website: "kim_naham",
+    comment: "부족한 점은 많지만 포기하지 않고 계속 노력하여 성장하는 아기사자가 되겠습니다!",
+  },
+  {
+    name: "백태우",
+    role: "Frontend",
+    intro: "I'm Empty Stack Junior :(",
+    image: taewoo,
+    badge: "AI/NLP",
+    email: "btu0414@gmail.com",
+    phone: "010-4564-4725",
+    website: "https://www.acmicpc.net",
+    comment: "모두가 원하는 개발자가 되어보겠습니다.",
+  },
+  {
+    name: "정소민",
+    role: "Frontend",
+    intro: "컴퓨터공학과 25학번 정소민입니다.",
+    image: somin,
+    badge: "React Native",
+    email: "sominjung1116@gmail.com",
+    phone: "010-5615-8474",
+    website: "https://www.instagram.com/__z1siimhttps://example.com",
+    comment: "열심히 하겠습니다.",
+  },
+  {
+    name: "이도은",
+    role: "Frontend",
+    intro: "열심히 배우는 프론트엔드 개발자입니다!",
+    image: doeun,
+    badge: "React",
+    email: "dodo55860@gmail.com",
+    phone: "010-2686-5586",
+    comment: "팀원들에게 도움이 되는 개발자가 되고 싶습니다.",
+  },
+  {
+    name: "정서윤",
+    role: "Frontend",
+    intro: "준비중...",
+    image: seoyun,
+    badge: "HTML/CSS",
+    email: "example@gmail.com",
+    phone: "010-1234-5678",
+    website: "https://example.com",
+    comment: "준비중...",
+  },
+  {
+    name: "이태엽",
+    role: "Frontend",
+    intro: "준비중...",
+    image: taeyeop,
+    badge: "TypeScript",
+    email: "example@gmail.com",
+    phone: "010-1234-5678",
+    website: "https://example.com",
+    comment: "준비중...",
+  },
+];
